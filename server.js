c9b215ea48df0cd5d3490545e8339fcd1f36a336
Jaap <jaap@mrproper.dev>
2021-02-09 14:41:01 +0100

    Add lobby support

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const WebSocket = require('ws');
 const uuid = require('uuid');
+
+let Lobbies = [];
+
+
 const wss = new WebSocket.Server({
     port: 8080
 }, () => {
     console.log(`Server started`);
 });
 setInterval(() => {
-    console.log(`Online clients: ${wss.clients.size}`)
+    console.log(`Online clients: ${wss.clients.size}`);
+   // console.log(Lobbies)
 }, 1000)
 
 wss.on('connection', (ws, req) => {
@@ -17,22 +22,38 @@ wss.on('connection', (ws, req) => {
         id: ws.clientID
     }));
     ws.on('message', (data) => {
+        data = JSON.parse(data);
         data.ID = ws.clientID;
-        // console.log(`Data recieved ${data}`);
-        wss.clients.forEach(function each(client) {
-            if (client != ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
+        data.LobbyID = uuid.NIL;
+        if (data.type != "PlayerInfo") {
+            console.log(data)
+        }
+        if (data.type == "travelMap") {
+            data.LobbyID = addToLobby(ws, data.map).UUID;
+        } else {
+            const lobby = getLobby(data.LobbyID);
+            if (lobby == undefined) return;
+
+            // console.log(`Data recieved ${data}`);
+            lobby.clients.forEach(function each(client) {
+                if (client != ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data));
+                }
+            });
+        }
     });
     ws.on('close', (data) => {
-        console.log(`${ws.clientID} left the server`)
-        wss.clients.forEach(function each(client) {
+        data = JSON.parse(data);
+        console.log(`${ws.clientID} left the server`);
+        const lobby = getLobby(data.LobbyID);
+        if (lobby == undefined) return;
+        lobby.clients.forEach(function each(client) {
             if (client != ws && client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify({
                     type: "Disconnect",
                     id: ws.clientID
                 }));
+                removeFromLobby(ws);
             }
         });
     })
@@ -41,3 +62,46 @@ wss.on('connection', (ws, req) => {
 wss.on('listening', () => {
     console.log("Server online on port 8080")
 });
+
+function getLobby(UUID) {
+    const lobby = Lobbies.filter(function (lobby) {
+        return lobby.UUID == UUID;
+    });
+    if (!lobby || lobby.length == 0) return undefined;
+    return lobby[0];
+}
+
+function removeFromLobby(user) {
+    for (let i = 0; i < Lobbies.length; i++) {
+        if (Lobbies[i].UUID == user.LobbyID) {
+            Lobbies[i].users = Lobbies[i].users.filter(function (el) {
+                return el.clientID != user.clientID;
+            });
+            return;
+        }
+    }
+}
+
+function addToLobby(user, mapName) {
+    for (let i = 0; i < Lobbies.length; i++) {
+        if (Lobbies[i].mapName.toLowerCase() == mapName.toLowerCase()) {
+            Lobbies[i].clients.push(user);
+            console.log(`${user.clientID} added to lobby: ${Lobbies[i].UUID}`);
+            return Lobbies[i];
+        }
+    }
+    CreateNewLobby(mapName);
+    return addToLobby(user, mapName);
+}
+
+function CreateNewLobby(mapName) {
+    const newLobby = {
+        UUID: uuid.v4(),
+        mapName: mapName,
+        startTime: Date.now(),
+        clients: []
+    }
+    Lobbies.push(newLobby);
+    console.log(`Lobby created: ${JSON.stringify(newLobby)}`)
+    return newLobby;
+}
