437432f11313278e2c1adbbe93387a517b872951
Jaap <jaap@mrproper.dev>
2021-02-09 13:57:49 +0100

    Add UUID to users

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 const WebSocket = require('ws');
+const uuid = require('uuid');
 const wss = new WebSocket.Server({
     port: 8080
 }, () => {
     console.log(`Server started`);
 });
+setInterval(() => {
+    console.log(`Online clients: ${wss.clients.size}`)
+}, 1000)
 
-wss.on('connection', (ws) => {
-    console.log(`New connection ${ws.listenerCount()}`)
+wss.on('connection', (ws, req) => {
+    ws.clientID = uuid.v4();
+    console.log(`New connection: ${ws.clientID}, ${wss.clients.size}`);
+    ws.send(JSON.stringify({
+        type: "UpdateID",
+        id: ws.clientID
+    }));
     ws.on('message', (data) => {
-     //  console.log(`Data recieved ${data}`);
+        data.ID = ws.clientID;
+        // console.log(`Data recieved ${data}`);
         wss.clients.forEach(function each(client) {
-            if (client == ws && client.readyState === WebSocket.OPEN) {
-              client.send(data);
+            if (client != ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
             }
-          });
+        });
     });
+    ws.on('close', (data) => {
+        console.log(`${ws.clientID} left the server`)
+        wss.clients.forEach(function each(client) {
+            if (client != ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({
+                    type: "Disconnect",
+                    id: ws.clientID
+                }));
+            }
+        });
+    })
 });
 
 wss.on('listening', () => {
